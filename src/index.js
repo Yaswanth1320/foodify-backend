3b070476c5fc23eed91f3b1af35fe48ecabbf575
Yaswanth1320 <yashwanth.paravathala@gmail.com>
2025-06-18 15:48:09 +0530

    deploy

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import { ENV } from "./config/env.js";
 import { db } from "./config/db.js";
 import { favoritesTable } from "./db/schema.js";
 import { and, eq } from "drizzle-orm";
+import job from "./config/cron.js";
 
 const app = express();
 const PORT = ENV.PORT || 3000;
 app.use(express.json());
+
+if (ENV.NODE_ENV == "production") job.start();
 
 app.get("/home", (req, res) => {
   res.send("Welcome to the Recipe API!");
